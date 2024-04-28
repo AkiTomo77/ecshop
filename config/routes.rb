06dcd8c80f1a32e8d022f71bc62fe1e121e81aa5
“akino” <ak001477tm@gmail.com>
2024-04-28 18:04:21 +0900

    10商品に関する機能顧客

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,10 @@ Rails.application.routes.draw do
   namespace :admin do
     resources :products, only: %i[index show new create edit update]
   end
+
+  scope module: :customer do
+    resources :products, only: %i[index show]
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get "shops/akitomo" => "shops#akitomo"
   root  "shops#akitomo"
